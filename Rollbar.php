eb391e4fa8063b7a77d2ceb1157a14211ae567d0
Danil Zakablukovskiy <danil.kabluk@gmail.com>
2018-08-10 16:10:42 +0800

    Breaking changes: updated versions Rollbar 1.5, PHP 7.1, Yii 2.0.15 (#22)
    * update rollbar to 1.5
    
    * added `enabled` option
    
    * min php version 7.1
    min yii version 2.0.15
    
    * refactor rollbar logging and remove error and fatal error handlers to log exception coming from Yii2 ErrorHandler.
    correctly set error level if an error is fatal

--- Rollbar.php
+++ Rollbar.php
@@ -2,12 +2,13 @@
 
 namespace baibaratsky\yii\rollbar;
 
-use \Rollbar\Rollbar as BaseRollbar;
+use Rollbar\Rollbar as BaseRollbar;
 use Yii;
 use yii\base\BaseObject;
 
 class Rollbar extends BaseObject
 {
+    public $enabled = true;
     public $accessToken;
     public $baseApiUrl = 'https://api.rollbar.com/api/1/';
     public $batchSize;
@@ -30,35 +31,29 @@ class Rollbar extends BaseObject
      * Format: ['name of the exception class', 'exception_property' => ['range', 'of', 'values], ...]
      */
     public $ignoreExceptions = [
-            ['yii\web\HttpException', 'statusCode' => [404]],
+        ['yii\web\HttpException', 'statusCode' => [404]],
     ];
 
     public function init()
     {
-        BaseRollbar::init(
-                [
-                        'access_token' => $this->accessToken,
-                        'base_api_url' => $this->baseApiUrl,
-                        'batch_size' => $this->batchSize,
-                        'batched' => $this->batched,
-                        'branch' => $this->branch,
-                        'code_version' => $this->codeVersion,
-                        'environment' => $this->environment,
-                        'host' => $this->host,
-                        'included_errno' => $this->includedErrno,
-                        'logger' => $this->logger,
-                        'person_fn' => $this->personFn,
-                        'root' => !empty($this->root) ? Yii::getAlias($this->root) : null,
-                        'scrub_fields' => $this->scrubFields,
-                        'timeout' => $this->timeout,
-                        'proxy' => $this->proxy,
-                        'enable_utf8_sanitization' => $this->enableUtf8Sanitization,
-                ],
-                false,
-                false,
-                false
-        );
-
-        parent::init();
+        BaseRollbar::init([
+            'enabled' => $this->enabled,
+            'access_token' => $this->accessToken,
+            'base_api_url' => $this->baseApiUrl,
+            'batch_size' => $this->batchSize,
+            'batched' => $this->batched,
+            'branch' => $this->branch,
+            'code_version' => $this->codeVersion,
+            'environment' => $this->environment,
+            'host' => $this->host,
+            'included_errno' => $this->includedErrno,
+            'logger' => $this->logger,
+            'person_fn' => $this->personFn,
+            'root' => !empty($this->root) ? Yii::getAlias($this->root) : null,
+            'scrub_fields' => $this->scrubFields,
+            'timeout' => $this->timeout,
+            'proxy' => $this->proxy,
+            'enable_utf8_sanitization' => $this->enableUtf8Sanitization,
+        ], false, false, false);
     }
 }
